60d24e9eeb757b2625a52de8b30d90fc05951fdb
mahmoudellabban <mahmoud.ellabban.7@gmail.com>
2024-04-04 22:45:20 +0200

    done!

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
-import React from 'react';
-import {Routes, Route} from "react-router-dom";
+import React, {useEffect} from 'react';
+import {Routes, Route, useLocation} from "react-router-dom";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Home from "./pages/Home";
 import PropertyDetails from "./pages/PropertyDetails";
 const App = () => {
+  const ScrollToTop = () => {
+    const { pathname } = useLocation();
+    useEffect(() => {
+      window.scrollTo(0, 0);
+    }, [pathname]);
+    return null;
+  };
   return (
     <div className='max-w-[1440px] mx-auto bg-white'>
       <Header />
+      <ScrollToTop />
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/property/:id' element={<PropertyDetails />} />
